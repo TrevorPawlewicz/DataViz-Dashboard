1b181f138fe02c07716b9ee93991f823423ef7e8
Trev <trevor.pawlewicz@gmail.com>
2020-02-16 15:16:55 -0500

    updated bar chart and main.js

--- js/barChart.js
+++ js/barChart.js
@@ -1,45 +1,128 @@
+BarChart = function(_parentElement, _variable, _title){
+    this.parentElement = _parentElement;
+    this.variable = _variable;
+    this.title = _title;
 
-var svg = d3.select("svg"),
-    margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom;
+    this.initVis();
+};
 
-var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-    y = d3.scaleLinear().rangeRound([height, 0]);
+BarChart.prototype.initVis = function(){
+    var vis = this;
 
-var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    vis.margin = { left:60, right:50, top:30, bottom:30 };
+    vis.height = 130 - vis.margin.top - vis.margin.bottom;
+    vis.width = 350 - vis.margin.left - vis.margin.right;
 
-d3.tsv("data.tsv").then(function(d) {
-  d.frequency = +d.frequency;
-  return d;
-}, function(error, data) {
-  if (error) throw error;
+    vis.svg = d3.select(vis.parentElement)
+        .append("svg")
+        .attr("width", vis.width + vis.margin.left + vis.margin.right)
+        .attr("height", vis.height + vis.margin.top + vis.margin.bottom);
+    vis.g = vis.svg.append("g")
+        .attr("transform", "translate(" + vis.margin.left +
+            ", " + vis.margin.top + ")");
 
-  x.domain(data.map(function(d) { return d.letter; }));
-  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+    vis.t = () => { return d3.transition().duration(1000); }
 
-  g.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+    vis.linePath = vis.g.append("path")
+        .attr("class", "line")
+        .attr("fill", "none")
+        .attr("stroke-width", "3px");
 
-  g.append("g")
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y).ticks(10, "%"))
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("Frequency");
+    // vis.color = d3.scaleOrdinal(d3.schemeGreys[4]);
 
-  g.selectAll(".bar")
-    .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.letter); })
-      .attr("y", function(d) { return y(d.frequency); })
-      .attr("width", x.bandwidth())
-      .attr("height", function(d) { return height - y(d.frequency); });
-});
+    vis.x = d3.scaleBand()
+        .domain(["electronics", "furniture", "appliances", "materials"])
+        .range([0, vis.width])
+        .padding(0.5);
+
+    vis.y = d3.scaleLinear().range([vis.height, 0]);
+
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    vis.yAxisCall = d3.axisLeft()
+        .ticks(4);
+    vis.xAxisCall = d3.axisBottom()
+        .tickFormat(function(d) {
+            return "" + capitalizeFirstLetter(d);
+        });
+    vis.xAxis = vis.g.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + vis.height +")");
+    vis.yAxis = vis.g.append("g")
+        .attr("class", "y axis");
+
+    vis.g.append("text")
+        .attr("class", "title")
+        .attr("y", -15)
+        .attr("x", -50)
+        .attr("font-size", "12px")
+        .attr("text-anchor", "start")
+        .text(vis.title)
+
+    vis.wrangleData();
+};
+
+
+BarChart.prototype.wrangleData = function(){
+    var vis = this;
+
+    vis.dataFiltered = nestedCalls.map(function(category){
+        return {
+            category: category.key,
+            size: (category.values.reduce(function(accumulator, current){
+                return accumulator + current[vis.variable]
+            }, 0) / category.values.length)
+        }
+    })
+
+    vis.updateVis();
+};
+
+
+BarChart.prototype.updateVis = function(){
+    var vis = this;
+
+    // Update scales
+    vis.y.domain([0, d3.max(vis.dataFiltered, (d) => { return +d.size; })]);
+
+    // Update axes
+    vis.xAxisCall.scale(vis.x);
+    vis.xAxis.transition(vis.t()).call(vis.xAxisCall);
+    vis.yAxisCall.scale(vis.y);
+    vis.yAxis.transition(vis.t()).call(vis.yAxisCall);
+
+    // JOIN new data with old elements.
+    vis.rects = vis.g.selectAll("rect").data(vis.dataFiltered, function(d){
+        return d.category;
+    });
+
+    // EXIT old elements not present in new data.
+    vis.rects.exit()
+        .attr("class", "exit")
+        .transition(vis.t())
+        .attr("height", 0)
+        .attr("y", vis.height)
+        .style("fill-opacity", "0.1")
+        .remove();
+
+    // UPDATE old elements present in new data.
+    vis.rects.attr("class", "update")
+        .transition(vis.t())
+            .attr("y", function(d){ return vis.y(d.size); })
+            .attr("height", function(d){ return (vis.height - vis.y(d.size)); })
+            .attr("x", function(d){ return vis.x(d.category) })
+            .attr("width", vis.x.bandwidth)
+
+    // ENTER new elements present in new data.
+    vis.rects.enter()
+        .append("rect")
+        .attr("class", "enter")
+        .attr("y", function(d){ return vis.y(d.size); })
+        .attr("height", function(d){ return (vis.height - vis.y(d.size)); })
+        .attr("x", function(d){ return vis.x(d.category) })
+        .attr("width", vis.x.bandwidth)
+        .attr("fill", "grey")
+};
